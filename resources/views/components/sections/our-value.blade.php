2ae7f4effc0f43339d7de8a032c1f834f4353596
EnzoAmarilla <enzo100amarilla@gmail.com>
2024-03-15 13:51:00 -0300

    zoom imagenes y centrado txt

--- resources/views/components/sections/our-value.blade.php
+++ resources/views/components/sections/our-value.blade.php
@@ -1,71 +1,61 @@
 <div class="section-md bg-grey">
     <h4 class="title-hero-black text-center mb-5">Our Value Proposition</h4>
     <div class="container p-80">
-        <div class="row align-items-center justify-content-between">
+        <div class="d-flex justify-content-between">
             
-            <div class="col-lg-2">
-                <!-- Snippet 1 -->
-                <div class="hoverbox-9">
-                    <img src="{{ $img1 }}" alt="">
+            <!-- Snippet 1 -->
+            <div class="hoverbox-9 mr-2">
+                <img src="{{ $img1 }}" class="zoom-img">
+                <div class="content">
                     <span class="font-family-tertiary text-white font-weight-bold card-txt margin-0">Design</span>
                 </div>
-                <!-- end Snippet -->
             </div>
+            <!-- end Snippet -->
 
-            <div class="col-lg-2">
-                <!-- Snippet 2 -->
-                <div class="hoverbox-9">
-                    <img src="{{ $img2 }}" alt="">
-                    <div class="content">
-                        <span class="font-family-tertiary font-weight-bold card-txt margin-0">Permitting</span>
-                    </div>
+            <!-- Snippet 2 -->
+            <div class="hoverbox-9 mr-2">
+                <img src="{{ $img2 }}" class="zoom-img">
+                <div class="content">
+                    <span class="font-family-tertiary font-weight-bold card-txt margin-0">Permitting</span>
                 </div>
-                <!-- end Snippet -->
             </div>
+            <!-- end Snippet -->
 
-            <div class="col-lg-2">
-                <!-- Snippet 3 -->
-                <div class="hoverbox-9">
-                    <img src="{{ $img3 }}" alt="">
-                    <div class="content">
-                        <span class="font-family-tertiary font-weight-bold card-txt margin-0">Construction</span>
-                    </div>
+            <!-- Snippet 3 -->
+            <div class="hoverbox-9 mr-2">
+                <img src="{{ $img3 }}" class="zoom-img">
+                <div class="content">
+                    <span class="font-family-tertiary font-weight-bold card-txt margin-0">Construction</span>
                 </div>
-                <!-- end Snippet -->
             </div>
+            <!-- end Snippet -->
 
-            <div class="col-lg-2">
-                <!-- Snippet 4 -->
-                <div class="hoverbox-9">
-                    <img src="{{ $img4 }}" alt="">
-                    <div class="content">
-                        <span class="font-family-tertiary font-weight-bold card-txt margin-0">Connection</span>
-                    </div>
+            <!-- Snippet 4 -->
+            <div class="hoverbox-9 mr-2">
+                <img src="{{ $img4 }}" class="zoom-img">
+                <div class="content">
+                    <span class="font-family-tertiary font-weight-bold card-txt margin-0">Connection</span>
                 </div>
-                <!-- end Snippet -->
             </div>
+            <!-- end Snippet -->
 
-            <div class="col-lg-2">
-                <!-- Snippet 5 -->
-                <div class="hoverbox-9">
-                    <img src="{{ $img5 }}" alt="">
-                    <div class="content">
-                        <span class="font-family-tertiary font-weight-bold card-txt margin-0">Monitoring</span>
-                    </div>
+            <!-- Snippet 5 -->
+            <div class="hoverbox-9 mr-2">
+                <img src="{{ $img5 }}" class="zoom-img">
+                <div class="content">
+                    <span class="font-family-tertiary font-weight-bold card-txt margin-0">Monitoring</span>
                 </div>
-                <!-- end Snippet -->
             </div>
+            <!-- end Snippet -->
 
-            <div class="col-lg-2">
-                <!-- Snippet 6 -->
-                <div class="hoverbox-9">
-                    <img src="{{ $img6 }}" alt="">
-                    <div class="content">
-                        <span class="font-family-tertiary font-weight-bold card-txt margin-0">Financing</span>
-                    </div>
+            <!-- Snippet 6 -->
+            <div class="hoverbox-9">
+                <img src="{{ $img6 }}" class="zoom-img">
+                <div class="content">
+                    <span class="font-family-tertiary font-weight-bold card-txt margin-0">Financing</span>
                 </div>
-                <!-- end Snippet -->
             </div>
+            <!-- end Snippet -->
 
         </div>
         <div class="row align-items-center justify-content-center mt-3">
